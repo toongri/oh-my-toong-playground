0e87953bf310c8960a39642c06abc5932c354987
toongri <jhkim03284@gmail.com>
2026-02-26 04:25:18 +0900

    chore: Bun 루트 설정 추가 및 npm 설정 일괄 삭제

--- scripts/lib/dist/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * oh-my-toong Shared Library
- * Re-exports commonly used modules
- */
-// Task reader module
-export { readTasksFromDirectory, countIncompleteTasks, getInProgressTask, } from './task-reader.js';
-// Logging module
-export { initLogger, logDebug, logInfo, logWarn, logError, logStart, logEnd, LogLevel, } from './logging.js';
